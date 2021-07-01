e1fa36ac886d652f26717ba4c18560181f41e94b
daryl <susecjh@gmail.com>
2021-07-02 00:49:00 +0800

    Feature: namespace query support id and description

--- service/namespace/query.go
+++ service/namespace/query.go
@@ -2,6 +2,7 @@ package namespace
 
 import (
 	"fmt"
+
 	"github.com/goools/tools/errorx"
 	"github.com/siskinc/srv-name-list/contants/error_code"
 	"github.com/siskinc/srv-name-list/models"
@@ -10,10 +11,12 @@ import (
 )
 
 type QueryNamespaceReq struct {
-	Code        *string `form:"code"`         // Code 命名空间code
+	ID          *string `form:"id"`           // ID 命名空间ID（精确查询）
+	Code        *string `form:"code"`         // Code 命名空间code（模糊查询）
+	Description *string `form:"description"`  // Description 命名空间的描述（模糊查询）
 	PageIndex   int64   `form:"page_index"`   // PageIndex 页码
 	PageSize    int64   `form:"page_size"`    // PageSize 分页数量
-	SortedField string `form:"sorted_field"` // SortedField 排序字段
+	SortedField string  `form:"sorted_field"` // SortedField 排序字段
 }
 
 func (service *Service) Query(req *QueryNamespaceReq) (result []*models.Namespace, total int64, err error) {
@@ -22,10 +25,29 @@ func (service *Service) Query(req *QueryNamespaceReq) (result []*models.Namespac
 		return
 	}
 	filter := bson.D{}
+	if req.ID != nil && *req.ID != "" {
+		var oid primitive.ObjectID
+		oid, err = primitive.ObjectIDFromHex(*req.ID)
+		if err != nil {
+			err = errorx.NewError(error_code.CustomForbiddenParameterInvalid, fmt.Errorf("id is invalid"))
+			return
+		}
+		filter = append(filter, bson.E{
+			Key:   "_id",
+			Value: oid,
+		})
+	}
 	if req.Code != nil && *req.Code != "" {
 		regexFilter := bson.E{
 			Key:   "code",
-			Value: bson.D{{"$regex", primitive.Regex{Pattern: fmt.Sprintf("%s", *req.Code), Options: "i"}}},
+			Value: bson.D{{Key: "$regex", Value: primitive.Regex{Pattern: *req.Code, Options: "i"}}},
+		}
+		filter = append(filter, regexFilter)
+	}
+	if req.Description != nil && *req.Description != "" {
+		regexFilter := bson.E{
+			Key:   "description",
+			Value: bson.D{{Key: "$regex", Value: primitive.Regex{Pattern: *req.Description, Options: "i"}}},
 		}
 		filter = append(filter, regexFilter)
 	}
